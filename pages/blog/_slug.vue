7ab0b7a6051c96242cf819b9de874fa79d50c5d5
Thibault Stringat <thibault.stringat@gmail.com>
2020-10-08 13:49:10 +0200

    optimizations

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -41,15 +41,8 @@
 export default {
   async asyncData({ $content, params }) {
     const article = await $content('articles', params.slug).fetch()
-    const [prev, next] = await $content('articles')
-      .only(['title', 'slug'])
-      .sortBy('createdAt', 'asc')
-      .surround(params.slug)
-      .fetch()
     return {
-      article,
-      prev,
-      next
+      article
     }
   },
   methods: {
@@ -58,6 +51,11 @@ export default {
       return new Date(date).toLocaleDateString('en', options)
     }
   }
+  // ,head() {
+  //   return {
+  //     title: this.article.title
+  //   }
+  // }
 }
 </script>
 <style>
