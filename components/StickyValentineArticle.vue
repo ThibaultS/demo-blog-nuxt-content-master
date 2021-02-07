83e68b6593341dd88418a827b6aeda3b14a9b2b5
Thibault Stringat <thibault.stringat@gmail.com>
2021-02-07 18:44:16 +0100

    adapt recipe page

---
+++ components/StickyValentineArticle.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-row>
+    <b-col>
+      <b-card
+        class="push-category push-event push-valentin mb-4 pb-8 pt-8 text-center"
+      >
+        <b-card-title title-tag="h3">
+          Mes desserts pour la Saint-Valentin 2021 !
+        </b-card-title>
+        <nuxt-link
+          role="button"
+          target="_self"
+          to="/categories/saint-valentin"
+          variant="primary"
+          class="btn btn-light mt-4"
+        >
+          Découvrir
+        </nuxt-link>
+      </b-card>
+    </b-col>
+  </b-row>
+</template>
+<script>
+export default {}
+</script>
